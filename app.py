cad22ab14deeef49cc137251ec83a75b9fb66ebe
“JOSEPH <joseph.griffin@hellofresh.com>
2023-10-26 21:32:18 +0200

    favicon and title

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import dash_bootstrap_components as dbc
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(
         __name__,
+        title='Wandering Griffin Travel',
         external_stylesheets=[dbc.themes.MORPH],
         suppress_callback_exceptions=True
         )
